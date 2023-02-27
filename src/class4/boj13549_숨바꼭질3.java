b114acbf4c85596065baf1a6513a570d1352b44c
seungyeon38 <seongyeon38@gmail.com>
2023-02-27 12:38:28 +0900

    boj13549_숨바꼭질3

---
+++ src/class4/boj13549_숨바꼭질3.java
@@ -0,0 +1,58 @@
+package class4;
+
+import java.util.*;
+import java.io.*;
+
+public class boj13549_숨바꼭질3 {
+	static boolean[] visited;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken()); // 0~100000
+		int K = Integer.parseInt(st.nextToken()); // 0~100000
+		int min = 0; 
+		int max = 100000;
+		visited = new boolean[100001];
+		
+		PriorityQueue<Node> pq = new PriorityQueue<Node>();
+		pq.offer(new Node(N, 0));
+		
+		while(!pq.isEmpty()) {
+			Node n = pq.poll();
+			
+			if(n.loc == K) {
+				System.out.println(n.time);
+				return; 
+			}
+			
+			visited[n.loc] = true; 
+			
+			if(n.loc+1 <= max && !visited[n.loc+1]) {
+				pq.offer(new Node(n.loc+1, n.time+1));
+			} 
+			if(n.loc-1>=0 && n.loc-1<=max && !visited[n.loc-1]) {
+				pq.offer(new Node(n.loc-1, n.time+1));
+			}
+			if(n.loc*2 <= max && !visited[n.loc*2]) {
+				pq.offer(new Node(n.loc*2, n.time));
+			} 
+		}
+	}
+	
+	public static class Node implements Comparable<Node>{
+		int loc; 
+		int time;
+		
+		Node(int loc, int time) {
+			this.loc = loc;
+			this.time = time; 
+		}
+		
+		@Override 
+		public int compareTo(Node n) {
+			return this.time - n.time;
+		}
+	}
+}
