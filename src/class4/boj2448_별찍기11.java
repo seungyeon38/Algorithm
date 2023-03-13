512a2ad7c06e6d0a4e3fe468fb01bcf233147083
seungyeon38 <seongyeon38@gmail.com>
2023-03-13 14:51:27 +0900

    boj15652_N과M4

--- src/class4/boj2448_별찍기11.java
+++ src/class4/boj2448_별찍기11.java
@@ -1,36 +1,36 @@
-package class4;
-
-import java.util.*;
-import java.io.*;
-
-public class boj2448_별찍기11 {
-	static int N;
-	static String[] grid;
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine()); // 3*2k(0~10)
-		
-		grid = new String[N];
-		grid[0] = "  *  ";
-		grid[1] = " * * ";
-		grid[2] = "*****";
-		
-		// n+1 (N-1)
-		// (n+1)*2 (N-1)*2
-		for(int n=0; n<N; n++) {
-			
-		}
-	}
-	
-	public static void draw(int n) {
-		int recur = n/3;
-		int v = n%3;
-		
-		for(int i=0; i<recur; i++) {
-			grid[n] = 
-		}
-		
-	}
-}
+//package class4;
+//
+//import java.util.*;
+//import java.io.*;
+//
+//public class boj2448_별찍기11 {
+//	static int N;
+//	static String[] grid;
+//	
+//	public static void main(String[] args) throws Exception{
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		N = Integer.parseInt(br.readLine()); // 3*2k(0~10)
+//		
+//		grid = new String[N];
+//		grid[0] = "  *  ";
+//		grid[1] = " * * ";
+//		grid[2] = "*****";
+//		
+//		// n+1 (N-1)
+//		// (n+1)*2 (N-1)*2
+//		for(int n=0; n<N; n++) {
+//			
+//		}
+//	}
+//	
+//	public static void draw(int n) {
+//		int recur = n/3;
+//		int v = n%3;
+//		
+//		for(int i=0; i<recur; i++) {
+//			grid[n] = 
+//		}
+//		
+//	}
+//}
