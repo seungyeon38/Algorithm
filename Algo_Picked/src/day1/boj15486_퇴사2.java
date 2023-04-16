63c638977a873c4327cd0cc98de3b42a1b6cd2ab
seungyeon38 <seongyeon38@gmail.com>
2023-04-16 21:04:08 +0900

    boj15486_퇴사2

---
+++ Algo_Picked/src/day1/boj15486_퇴사2.java
@@ -0,0 +1,37 @@
+package day1;
+
+import java.util.*;
+import java.io.*;
+
+public class boj15486_퇴사2 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine()); // 1~1500000
+		
+		int[][] consults = new int[N][2]; // [시간, 금액]
+		
+		for(int i=0; i<N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			consults[i][0] = Integer.parseInt(st.nextToken()); // 상담 기간 1~50
+			consults[i][1] = Integer.parseInt(st.nextToken()); // 상담 금액 1~1000
+		}
+		
+		int[] results = new int[N+1];
+		int max = 0;
+		
+		for(int i=0; i<N; i++) {
+			max = Math.max(max, results[i]);
+			
+			int next = i+consults[i][0];
+			
+			if(next <= N) {
+				results[next] = Math.max(results[next], max+consults[i][1]);
+			}
+		}
+		
+		max = Math.max(max, results[N]);
+		
+		System.out.println(max);
+	}
+}
