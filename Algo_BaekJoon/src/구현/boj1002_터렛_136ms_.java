bd49e75d42a708778bd410da05d1d5c9e48d1e95
seungyeon38 <seongyeon38@gmail.com>
2023-08-08 00:27:17 +0900

    boj1002_터렛

--- Algo_BaekJoon/src/구현/boj1002_터렛_136ms_.java
+++ Algo_BaekJoon/src/구현/boj1002_터렛_136ms_.java
@@ -3,11 +3,12 @@ package 구현;
 import java.io.*;
 import java.util.*;
 
-public class boj1002_터렛 {
+public class boj1002_터렛_136ms_ {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		
 		for(int t=0; t<T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,13 +20,20 @@ public class boj1002_터렛 {
 			int y2 = Integer.parseInt(st.nextToken());
 			int r2 = Integer.parseInt(st.nextToken());
 			
-			int r_p1 = x1+r1;
-			int u_p1 = y1+r1;
-			int l_p1 = x1-r1;
-			int d_p1 = y1-r1;
+			// 중점 사이의 거리보다 클 때, 안에 있을 때(0) | 외접, 내접(1) | 두 점에서 만날 때(2) | 같을 때 (-1)
+			int dist = (int)(Math.pow(x1-x2, 2)+Math.pow(y1-y2, 2));
 			
-			// 조규현 기준 류재명이 어디 있는지 판단 
-			
+			if(x1==x2 && y1==y2 && r1==r2) { // 같을 때 -1  
+				sb.append("-1\n");
+			} else if(dist>Math.pow(r1+r2, 2) || dist<Math.pow(r2-r1, 2)) { // 클 때, 안에 있을 때 0
+				sb.append("0\n");
+			} else if(dist==Math.pow(r1+r2, 2) || dist==Math.pow(r2-r1, 2)){ // 외접, 내접 1
+				sb.append("1\n");
+			} else {
+				sb.append("2\n");
+			}
 		}
+		
+		System.out.println(sb.toString());
 	}
 }
